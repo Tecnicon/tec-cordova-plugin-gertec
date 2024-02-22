f5f2431a83d93bde7fe0194e300c04864e7d94f6
Tecnicon <96908333+Tecnicon@users.noreply.github.com>
2024-02-22 14:37:47 -0300

    ajustes

--- www/gertec.js
+++ www/gertec.js
@@ -6,6 +6,14 @@ gertec.imprimir = function (text, onSuccess, onFail) {
     if (typeof text === "undefined" || text === null) text = "";
 	cordova.exec(onSuccess, onFail, "gertec", "imprimir", [text]);
 };
+gertec.inserir = function (text, onSuccess, onFail) {
+    if (typeof text === "undefined" || text === null) text = "";
+	cordova.exec(onSuccess, onFail, "gertec", "inserir", [text]);
+};
+gertec.aproximar = function (text, onSuccess, onFail) {
+    if (typeof text === "undefined" || text === null) text = "";
+	cordova.exec(onSuccess, onFail, "gertec", "aproximar", [text]);
+};
 
 var gertec = new gertec();
 module.exports = gertec;
