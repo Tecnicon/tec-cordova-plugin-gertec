e69bf31fbc942adf740c5934501ae0a0413629ca
Tecnicon <96908333+Tecnicon@users.noreply.github.com>
2024-02-22 13:39:38 -0300

    ppcomp

--- src/android/gertec.java
+++ src/android/gertec.java
@@ -13,6 +13,7 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.widget.Toast;
 import android.content.Context;
+import android.app.Activity;
 
 import br.com.gertec.gedi.enums.GEDI_PRNTR_e_PrintDensity;
 import br.com.gertec.gedi.exceptions.GediException;
@@ -21,30 +22,62 @@ import br.com.gertec.gedi.interfaces.IPRNTR;
 import br.com.gertec.gedi.structs.GEDI_PRNTR_st_StringConfig;
 import br.com.gertec.gedi.GEDI;
 import java.lang.Exception;
-//import br.com.gertec.ppcomp.PPComp;
+
+import br.com.gertec.ppcomp.PPComp;
+import br.com.gertec.ppcomp.PPCompException;
+import br.com.gertec.ppcomp.exceptions.PPCompCancelException;
+import br.com.gertec.ppcomp.exceptions.PPCompNotifyException;
+import br.com.gertec.ppcomp.exceptions.PPCompProcessingException;
+import br.com.gertec.ppcomp.exceptions.PPCompTabExpException;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class gertec extends CordovaPlugin {
 
-
- private Context context;
+    private Context context;
+    private CordovaInterface cordovaInt;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+
         super.initialize(cordova, webView);
         this.context = cordova.getActivity().getApplicationContext();
+        this.cordovaInt = cordova;
     }
-    
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("imprimir".equals(action)) {
-            
+
             String texto = args.toString();
 
-           try {
+            try {
                 imprimirComprovante(texto);
+            } catch (Exception e) {
+                callbackContext.error(e.getMessage());
+                return false;
+            }
+            callbackContext.success();
+            return true;
+        } else if ("inserir".equals(action)) {
+
+            String texto = args.toString();
+
+            try {
+                inserir();
+            } catch (Exception e) {
+                callbackContext.error(e.getMessage());
+                return false;
+            }
+            callbackContext.success();
+            return true;
+        } else if ("aproximar".equals(action)) {
+
+            String texto = args.toString();
+
+            try {
+                aproximar();
             } catch (Exception e) {
                 callbackContext.error(e.getMessage());
                 return false;
@@ -58,7 +91,6 @@ public class gertec extends CordovaPlugin {
     }
 
     private void imprimirComprovante(String texto) {
-       // PPComp ppComp;
         IGEDI mGedi;
         GEDI.init(context);
         mGedi = GEDI.getInstance(context);
@@ -74,11 +106,92 @@ public class gertec extends CordovaPlugin {
                 mPRNTR.SetPrintDensity(GEDI_PRNTR_e_PrintDensity.HIGH);
                 mPRNTR.DrawStringExt(strconfig, texto);
                 mPRNTR.Output();
-            //    mostrarMensagem("Comprovante impresso");
+                mostrarMensagem("Comprovante impresso");
             } catch (GediException e) {
 
-             //   mostrarMensagem("Erro ao imprimir comprovante: " + e);
+                mostrarMensagem("Erro ao imprimir comprovante: " + e);
             }
         }).start();
     }
+
+    private void inserir() {
+
+        PPComp ppComp = new PPComp(context);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Parametros de entrada do GetCard
+                String gcr_input = "0001000000001000691231210457012345678900";
+                String output = "";
+                StringBuffer msgNotify = new StringBuffer();
+
+                try {
+                    ppComp.PP_StartGetCard(gcr_input);
+                    while (true) {
+                        try {
+                            mostrarMensagem("Insira o cartão...");
+                            output = ppComp.PP_GetCard();
+                            mostrarMensagem("Resultado = " + output);
+                            break;
+                        } catch (PPCompProcessingException e) {
+                        } catch (PPCompNotifyException e) {
+                        } catch (PPCompTabExpException e) {
+                            ppComp.PP_ResumeGetCard();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void aproximar() {
+        PPComp ppComp = new PPComp(context);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Parametros de entrada do GoOnChip
+                String goc_input = "000000001000000000000000001101000000000000000000000000000000001000003E820000003E880000";
+                String goc_inputTags = "0019B";
+                String goc_inputTagsOpt = "0119F0B1F813A9F6B9F6C9F66";
+                String output = "";
+                try {
+                    ppComp.PP_StartGoOnChip(goc_input, goc_inputTags, goc_inputTagsOpt);
+                    while (true) {
+                        try {
+                            output = ppComp.PP_GoOnChip();
+                            mostrarMensagem("Resultado = " + output);
+                            cordovaInt.getActivity().finish();
+                         
+                            //  PinKBDActivity.getKBDData().activity.finish();
+                            //     imprimirComprovante(output);
+                            break;
+                        } catch (PPCompProcessingException e) {
+                        } catch (PPCompNotifyException e) {
+                        }
+                    }
+                } catch (Exception e) {
+
+                    mostrarMensagem("Erro no GOC" + e);
+                    cordovaInt.getActivity().finish();
+                 
+                    //  PinKBDActivity.getKBDData().activity.finish();
+                }
+            }
+        }).start();
+
+    }
+
+    private void mostrarMensagem(String texto) {
+        cordovaInt.getActivity().runOnUiThread(
+                new Runnable() {
+            public void run() {
+                Toast.makeText(context, texto, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
