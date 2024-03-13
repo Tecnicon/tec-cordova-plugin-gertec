8d6c8b9212904201f7396668096c4ec664ddfd30
Tecnicon <96908333+Tecnicon@users.noreply.github.com>
2024-03-13 09:51:20 -0300

    Update gertec.js

--- www/gertec.js
+++ www/gertec.js
@@ -12,14 +12,6 @@ gertec.inicializar = function (text, onSuccess, onFail) {
 	cordova.exec(onSuccess, onFail, "gertec", "inicializar", [text]);
 };
 
-gertec.aproximar = function (text, onSuccess, onFail) {
-    if (typeof text === "undefined" || text === null) text = "";
-	cordova.exec(onSuccess, onFail, "gertec", "aproximar", [text]);
-};
-
-
-
-
 var gertec = new gertec();
 module.exports = gertec;
 
